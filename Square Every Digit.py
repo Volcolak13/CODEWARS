c42a057eb76c24351e9eef5561b3689eb6d5aefa
Greg <Volcolak@gmail.com>
2022-06-04 00:54:45 +0300

    Task # 7 Done (Added solution from CW (failed))

--- Square Every Digit.py
+++ Square Every Digit.py
@@ -1,3 +1,21 @@
 x = 1111
 y = "".join(map(str, ([int(i)**2 for i in str(x)])))
 print(y)
+
+#   Inspired by Square Every Digit (and by the inimitable myjinxin2015's many clever one-line kata), your goal
+#   here is precisely the same (to square every digit in the given number), in 36 or fewer characters
+#   (JavaScript), or 47 or fewer in Ruby
+#
+# (Note: in Ruby, spaces do not count towards your total-- within reason. Spacing shouldn't be more than 30%
+# of your total to avoid possible cheats).
+#
+# Your return value should be in integer format.
+#
+# Your input will always be a valid, non-negative integer... no tricks!
+#
+# Bonus: Can you get it down to 34 characters? (43 in Ruby!)
+#
+# Note: Just as a head's up, numbers are > 2^31, so bitwise operators aren't viable.
+
+#   Решение с CODEWARS
+# sd=x=>+`${x}`.replace(/./g,a=>a*a)
